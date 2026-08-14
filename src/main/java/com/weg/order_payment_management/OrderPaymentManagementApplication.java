b2dad94163bb56d7e1c3d08d40022c2db9a5b937
SAMUEL DA SILVA MELLO <samuel_silva-mello@estudante.sesisenai.org.br>
2026-08-14 10:22:48 -0300

    feat: starting project

---
+++ src/main/java/com/weg/order_payment_management/OrderPaymentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.weg.order_payment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPaymentManagementApplication.class, args);
+	}
+
+}
